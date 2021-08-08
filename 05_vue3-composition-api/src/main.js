738e00db1b0e9bf72314ddcc66610394a6689924
luckyporo <565010808@qq.com>
2021-08-08 13:57:16 +0800

    update

--- 05_vue3-composition-api/src/main.js
+++ 05_vue3-composition-api/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 // import App from './03_ref-base/App.vue'
 // import App from './04_computed_watch/App.vue'
 // import App from './05_lifecycle/App.vue'
-import App from './06_provide-inject/App.vue'
+// import App from './06_provide-inject/App.vue'
+// import App from './07_practice/App.vue'
+import App from './08_no-setup/App.vue'
 
 createApp(App).mount('#app')
