8f8ae40554b4a0fdba9825a376c6b60c7b857d4c
luckyporo <565010808@qq.com>
2021-08-08 12:07:43 +0800

    update

--- 05_vue3-composition-api/src/04_computed_watch/App.vue
+++ 05_vue3-composition-api/src/04_computed_watch/App.vue
@@ -2,15 +2,21 @@
   <div>
     <h2>{{ fullName }}</h2>
     <button @click="changeName">changeName</button>
+    <h2>{{ message }}</h2>
+    <button @click="changeMessage">changeMessage</button>
+    <h2>{{ age }}</h2>
+    <button @click="changeAge">changeAge</button>
   </div>
 </template>
 <script>
-import { ref, computed } from 'vue'
+import { ref, computed, watchEffect, watch } from 'vue'
 
 export default {
   setup() {
     const firstName = ref('lucky')
     const lastName = ref('poro')
+    const message = ref('hello world');
+    const age = ref(18);
 
     const fullName = computed(() => firstName.value + lastName.value)
 
@@ -19,9 +25,29 @@ export default {
       lastName.value = 'san'
     }
 
+    const changeMessage = () => {
+      message.value = Math.floor(Math.random() * 100)
+    }
+
+    const changeAge = () => {
+      age.value = Math.floor(Math.random() * 30)
+    }
+
+    watchEffect(() => {
+      console.log('message:', message.value);
+    })
+
+    watch(age, (newVal, oldVal) => {
+      console.log(`newVal: ${newVal}, oldVal: ${oldVal}`)
+    })
+
     return {
       fullName,
-      changeName
+      changeName,
+      message,
+      changeMessage,
+      age,
+      changeAge
     }
   }
 
