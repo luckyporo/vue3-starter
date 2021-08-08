fa3f6e68faeff801626d936734db24d022937709
luckyporo <565010808@qq.com>
2021-08-08 13:51:15 +0800

    update

---
+++ 05_vue3-composition-api/src/05_lifecycle/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <button @click="increment">{{ counter }}</button>
+    <br>
+    <hello v-if="show" />
+    <br>
+    <button @click="showCopn">销毁/挂载子组件</button>
+  </div>
+</template>
+<script>
+import { ref, onMounted, onUpdated, onUnmounted } from 'vue'
+import Hello from './Hello.vue'
+
+export default {
+  components: {
+    Hello
+  },
+  setup() {
+    const counter = ref(0);
+    const show = ref(true);
+
+    const increment = () => {
+      counter.value++;
+    }
+
+    const showCopn = () => {
+      show.value = !show.value;
+    }
+
+    onMounted(() => {
+      console.log('App Mounted')
+    })
+
+    onUpdated(() => {
+      console.log('App Updated')
+    })
+
+    onUnmounted(() => {
+      console.log('App Unmounted')
+    })
+
+    return {
+      counter,
+      increment,
+      show,
+      showCopn
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
--- 05_vue3-composition-api/src/main.js
+++ 05_vue3-composition-api/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 // import App from './01_mixin-and-extends/App.vue'
 // import App from './02_composition-base/App.vue'
 // import App from './03_ref-base/App.vue'
-import App from './04_computed_watch/App.vue'
+// import App from './04_computed_watch/App.vue'
+// import App from './05_lifecycle/App.vue'
+import App from './06_provide-inject/App.vue'
 
 createApp(App).mount('#app')
